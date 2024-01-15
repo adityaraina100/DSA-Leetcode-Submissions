34f80daa0a92d2e330281ed0a3fa93bce0babefe
Aditya Raina <adityaraina788@gmail.com>
2024-01-15 15:25:20 +0530

    Add files via upload

---
+++ lengthoflastword.py
@@ -0,0 +1,9 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        words=s.split()
+        if words:
+            return len(words[-1])
+        else:
+            return 0
+
+        
